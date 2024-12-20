a298aa4809eda2206cad208e9f94b817d664c893
jangidas <jangidas056@gmail.com>
2024-12-20 20:19:27 +0530

    Link ohk

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -8,25 +8,48 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import { FaCartShopping } from "react-icons/fa6";
 import { CiUser } from "react-icons/ci";
 import "./about.css";
+import "./head.css";
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <>
       <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
-        <Container>
+        <Container className="linkss">
           <Navbar.Brand href="/">Pooja Novelty&Gifts</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="/">HOME</Nav.Link>
-              <Nav.Link href="/about">ABOUT</Nav.Link>
-              <Nav.Link href="/product">PRODUCT</Nav.Link>
+            <Nav className="me-auto ">
+              <Nav.Link>
+                <Link to={"/"} className="navi">
+                  HOME
+                </Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to={"/about"} className="navi">
+                  ABOUT
+                </Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to={"/product"} className="navi">
+                  PRODUCT
+                </Link>
+              </Nav.Link>
               <NavDropdown title="COSMATICS" id="collapsible-nav-dropdown">
-                <NavDropdown.Item href="/product"><Link to={'/product'}>LIPSTICS</Link></NavDropdown.Item>
-                <NavDropdown.Item href="/product">NAIL POLISH</NavDropdown.Item>
-                <NavDropdown.Item href="/product">
-                  CREAM POWDERS
+                <NavDropdown.Item>
+                  <Link to={"/product"} className="navi">
+                    LIPSTICS
+                  </Link>
+                </NavDropdown.Item>
+                <NavDropdown.Item>
+                  <Link to={"/product"} className="navi">
+                    NAIL POLISH
+                  </Link>
+                </NavDropdown.Item>
+                <NavDropdown.Item>
+                  <Link to={"/product"} className="navi">
+                    CREAM POWDERS
+                  </Link>
                 </NavDropdown.Item>
               </NavDropdown>
             </Nav>
@@ -34,8 +57,10 @@ const Header = () => {
               <Nav.Link href="/addcart">
                 <FaCartShopping className="icons" />
               </Nav.Link>
-              <Nav.Link eventKey={2} href="/login">
-                <CiUser className="icons" />
+              <Nav.Link eventKey={2}>
+                <Link to={"/login"} className="navi">
+                  <CiUser className="icons" />
+                </Link>
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
